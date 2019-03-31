7592dd688df659b4edf529f9af4e23e85459af60
yuzhijun <yuzhijun@bilibili.com>
2019-03-31 18:18:11 +0800

    对代码进行抽取重构

--- sniffer/src/main/groovy/com/bilibili/volador_sniffer/internal/procedure/StartupTimeConsumeProcedure.groovy
+++ sniffer/src/main/groovy/com/bilibili/volador_sniffer/internal/procedure/StartupTimeConsumeProcedure.groovy
@@ -1,27 +1,17 @@
 package com.bilibili.volador_sniffer.internal.procedure
 
-import com.android.SdkConstants
+
 import com.android.build.api.transform.*
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.BasePlugin
 import com.android.build.gradle.internal.TaskManager
 import com.android.utils.FileUtils
-import com.bilibili.volador_sniffer.internal.javaassit.Injector.IClassInjector
 import com.bilibili.volador_sniffer.internal.javaassit.Injector.Injectors
-import com.bilibili.volador_sniffer.internal.util.AutoTextUtil
 import com.bilibili.volador_sniffer.internal.util.Logger
 import com.bilibili.volador_sniffer.internal.util.StartupUtil
-import groovy.io.FileType
 import javassist.ClassPool
-import javassist.CtClass
 import org.apache.commons.codec.digest.DigestUtils
-import org.apache.commons.io.IOUtils
 import org.gradle.api.Project
-
-import java.util.jar.JarEntry
-import java.util.jar.JarFile
-import java.util.jar.JarOutputStream
-import java.util.zip.ZipEntry
 
 class StartupTimeConsumeProcedure extends  AbsProcedure{
     private def globalScope
@@ -78,17 +68,10 @@ class StartupTimeConsumeProcedure extends  AbsProcedure{
      */
     def doTransform(Collection<TransformInput> inputs,
                     TransformOutputProvider outputProvider, Context context) {
-//        BatchTaskScheduler taskScheduler = new BatchTaskScheduler()
-//        taskScheduler.addTask(new ITask(){
-//            @Override
-//            Object call() throws Exception {
-                /* 初始化 ClassPool */
-                Object pool = initClassPool(inputs)
-                Logger.info("||-->初始化classpool结束")
-                doInject(inputs, pool, outputProvider, context)
-//                return null
-//            }
-//        })
+           /* 初始化 ClassPool */
+           Object pool = initClassPool(inputs)
+
+           doInject(inputs, pool, outputProvider, context)
     }
 
     /**
@@ -99,7 +82,7 @@ class StartupTimeConsumeProcedure extends  AbsProcedure{
         try {
             inputs.each { TransformInput input ->
                 input.directoryInputs.each { DirectoryInput dirInput ->
-                        handleDir(pool, dirInput, outputProvider, context)
+                        StartupUtil.handleDir(pool, dirInput, outputProvider, context)
                 }
                 input.jarInputs.each { JarInput jarInput ->
                     String destName = jarInput.file.name
@@ -114,7 +97,7 @@ class StartupTimeConsumeProcedure extends  AbsProcedure{
                     File inputFile = null
                     Injectors.values().each {
                         inputFile = modifiedJar == null ? jarInput.file : modifiedJar
-                        modifiedJar = handleJar(pool, inputFile, it.injector, outputProvider, context)
+                        modifiedJar = StartupUtil.handleJar(pool, inputFile, it.injector, outputProvider, context)
                         if (modifiedJar == null) {
                             modifiedJar = jarInput.file
                         }
@@ -141,108 +124,6 @@ class StartupTimeConsumeProcedure extends  AbsProcedure{
             pool.insertClassPath(it)
         }
         pool
-    }
-
-    /**
-     * 处理 jar
-     */
-    static def handleJar(ClassPool pool, File jarFile, IClassInjector injector, TransformOutputProvider outputProvider, Context context) {
-        JarFile jar = new JarFile(jarFile)
-        def hexName = DigestUtils.md5Hex(jarFile.absolutePath).substring(0, 8)
-        def outputJar = new File(context.temporaryDir, hexName + jarFile.name)
-        Logger.info("||-->jar包中的文件: ${outputJar.absolutePath}")
-        JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(outputJar))
-        Enumeration<JarEntry> jarEntries = jar.entries()
-        while (jarEntries.hasMoreElements()) {
-            JarEntry jarEntry = jarEntries.nextElement()
-            InputStream inputStream = jar.getInputStream(jarEntry)
-            byte[] sourceClassBytes = IOUtils.toByteArray(inputStream)
-
-            String entryName = jarEntry.getName()
-            ZipEntry zipEntry = new ZipEntry(entryName)
-            jarOutputStream.putNextEntry(zipEntry)
-
-            CtClass ctCls = null
-            if (entryName.endsWith(SdkConstants.DOT_CLASS)) {
-                Logger.info("||-->jar包中的文件: ${entryName}")
-                String className = entryName.replace("/", ".").replace(".class", "")
-                try {
-                    Logger.info("||-->jar包中要处理的文件: ${className}")
-                    ctCls= injector.injectJar(pool, className, outputProvider, context)
-                } catch (Exception e) {
-                    //ignore.
-                    e.printStackTrace()
-                }
-            }
-
-            if (ctCls == null) {
-                jarOutputStream.write(sourceClassBytes)
-            } else {
-                jarOutputStream.write(ctCls.toBytecode())
-            }
-
-            jarOutputStream.closeEntry()
-        }
-
-        jarOutputStream.close()
-        jar.close()
-
-        return outputJar
-    }
-
-    /**
-     * 处理目录中的 class 文件
-     */
-    static def handleDir(ClassPool pool, DirectoryInput input, TransformOutputProvider outputProvider, Context context) {
-        FileOutputStream outputStream = null
-        CtClass ctCls = null
-        File dest = outputProvider.getContentLocation(input.name, input.contentTypes, input.scopes, Format.DIRECTORY)
-        def dir = input.file
-        FileUtils.copyDirectory(dir, dest)
-            if (dir){
-                dir.traverse (type: FileType.FILES, nameFilter: ~/.*\.class/){
-                    File classFile ->
-                        File modified = null
-                        Injectors.values().each {//由於inject只会操作不同的文件,所以这里不会出现操作被覆盖问题
-                            try{
-                                String className = AutoTextUtil.path2ClassName(classFile.absolutePath.replace(dir.absolutePath + File.separator, ""))
-                                ctCls = it.injector.injectClass(pool, className, outputProvider, context)
-                                if (ctCls == null){
-                                    //do nothing
-                                }else{
-                                    modified = new File(context.temporaryDir, className.replace('.', '') + '.class')
-                                    if (modified.exists()) {
-                                        modified.delete()
-                                    }
-                                    modified.createNewFile()
-                                    outputStream = new FileOutputStream(modified)
-                                    outputStream.write(ctCls.toBytecode())
-                                }
-                            }catch(Exception e){
-                                e.printStackTrace()
-                            }finally{
-                                try {
-                                    if (outputStream != null) {
-                                        outputStream.close()
-                                    }
-                                } catch (Exception e) {
-                                    e.printStackTrace()
-                                }
-                            }
-
-                            if (ctCls != null){
-                                String fullClassName = classFile.absolutePath.replace(dir.absolutePath, "")
-                                File target = new File(dest.absolutePath + fullClassName)
-                                if (target.exists()) {
-                                    target.delete()
-                                }
-                                if(modified != null){
-                                    FileUtils.copyFile(modified, target)
-                                }
-                            }
-                        }
-                }
-            }
     }
 
     /**
--- sniffer/src/main/groovy/com/bilibili/volador_sniffer/internal/util/StartupUtil.groovy
+++ sniffer/src/main/groovy/com/bilibili/volador_sniffer/internal/util/StartupUtil.groovy
@@ -1,11 +1,28 @@
 package com.bilibili.volador_sniffer.internal.util
 
+import com.android.SdkConstants
+import com.android.build.api.transform.Context
 import com.android.build.api.transform.DirectoryInput
+import com.android.build.api.transform.Format
 import com.android.build.api.transform.JarInput
 import com.android.build.api.transform.TransformInput
+import com.android.build.api.transform.TransformOutputProvider
 import com.android.build.gradle.internal.scope.GlobalScope
 import com.android.sdklib.IAndroidTarget
+import com.android.utils.FileUtils
+import com.bilibili.volador_sniffer.internal.javaassit.Injector.IClassInjector
+import com.bilibili.volador_sniffer.internal.javaassit.Injector.Injectors
+import groovy.io.FileType
+import javassist.ClassPool
+import javassist.CtClass
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.io.IOUtils
 import org.gradle.api.Project
+
+import java.util.jar.JarEntry
+import java.util.jar.JarFile
+import java.util.jar.JarOutputStream
+import java.util.zip.ZipEntry
 
 public class StartupUtil {
 
@@ -50,5 +67,107 @@ public class StartupUtil {
      */
     def static getAndroidJarPath(GlobalScope globalScope) {
         return globalScope.getAndroidBuilder().getTarget().getPath(IAndroidTarget.ANDROID_JAR)
+    }
+
+    /**
+     * 处理目录中的 class 文件
+     */
+    static def handleDir(ClassPool pool, DirectoryInput input, TransformOutputProvider outputProvider, Context context) {
+        FileOutputStream outputStream = null
+        CtClass ctCls = null
+        File dest = outputProvider.getContentLocation(input.name, input.contentTypes, input.scopes, Format.DIRECTORY)
+        def dir = input.file
+        FileUtils.copyDirectory(dir, dest)
+        if (dir){
+            dir.traverse (type: FileType.FILES, nameFilter: ~/.*\.class/){
+                File classFile ->
+                    File modified = null
+                    Injectors.values().each {//由於inject只会操作不同的文件,所以这里不会出现操作被覆盖问题
+                        try{
+                            String className = AutoTextUtil.path2ClassName(classFile.absolutePath.replace(dir.absolutePath + File.separator, ""))
+                            ctCls = it.injector.injectClass(pool, className, outputProvider, context)
+                            if (ctCls == null){
+                                //do nothing
+                            }else{
+                                modified = new File(context.temporaryDir, className.replace('.', '') + '.class')
+                                if (modified.exists()) {
+                                    modified.delete()
+                                }
+                                modified.createNewFile()
+                                outputStream = new FileOutputStream(modified)
+                                outputStream.write(ctCls.toBytecode())
+                            }
+                        }catch(Exception e){
+                            e.printStackTrace()
+                        }finally{
+                            try {
+                                if (outputStream != null) {
+                                    outputStream.close()
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace()
+                            }
+                        }
+
+                        if (ctCls != null){
+                            String fullClassName = classFile.absolutePath.replace(dir.absolutePath, "")
+                            File target = new File(dest.absolutePath + fullClassName)
+                            if (target.exists()) {
+                                target.delete()
+                            }
+                            if(modified != null){
+                                FileUtils.copyFile(modified, target)
+                            }
+                        }
+                    }
+            }
+        }
+    }
+
+    /**
+     * 处理 jar
+     */
+    static def handleJar(ClassPool pool, File jarFile, IClassInjector injector, TransformOutputProvider outputProvider, Context context) {
+        JarFile jar = new JarFile(jarFile)
+        def hexName = DigestUtils.md5Hex(jarFile.absolutePath).substring(0, 8)
+        def outputJar = new File(context.temporaryDir, hexName + jarFile.name)
+        Logger.info("||-->jar包中的文件: ${outputJar.absolutePath}")
+        JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(outputJar))
+        Enumeration<JarEntry> jarEntries = jar.entries()
+        while (jarEntries.hasMoreElements()) {
+            JarEntry jarEntry = jarEntries.nextElement()
+            InputStream inputStream = jar.getInputStream(jarEntry)
+            byte[] sourceClassBytes = IOUtils.toByteArray(inputStream)
+
+            String entryName = jarEntry.getName()
+            ZipEntry zipEntry = new ZipEntry(entryName)
+            jarOutputStream.putNextEntry(zipEntry)
+
+            CtClass ctCls = null
+            if (entryName.endsWith(SdkConstants.DOT_CLASS)) {
+                Logger.info("||-->jar包中的文件: ${entryName}")
+                String className = entryName.replace("/", ".").replace(".class", "")
+                try {
+                    Logger.info("||-->jar包中要处理的文件: ${className}")
+                    ctCls= injector.injectJar(pool, className, outputProvider, context)
+                } catch (Exception e) {
+                    //ignore.
+                    e.printStackTrace()
+                }
+            }
+
+            if (ctCls == null) {
+                jarOutputStream.write(sourceClassBytes)
+            } else {
+                jarOutputStream.write(ctCls.toBytecode())
+            }
+
+            jarOutputStream.closeEntry()
+        }
+
+        jarOutputStream.close()
+        jar.close()
+
+        return outputJar
     }
 }
